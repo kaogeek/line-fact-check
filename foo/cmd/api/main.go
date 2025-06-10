d03b36d82499c92b4395fb0e915e0013dbfef42e
Prem Phansuriyanon <artdrawin@gmail.com>
2025-06-10 23:32:29 +0700

    add pillars package for 5 pillars of observability

--- foo/cmd/api/main.go
+++ foo/cmd/api/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"log/slog"
 	"net/http"
+	"time"
+
+	"github.com/kaogeek/line-fact-check/pillars"
 )
 
 func main() {
+	const (
+		name = "foo-api"
+		addr = ":8080"
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", pillars.HandlerEcho(name))
+	mux.Handle("/health", pillars.HandlerOk(name))
+
+	srv := http.Server{
+		Addr:         addr,
+		Handler:      mux,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 2,
+	}
+
 	slog.Info("listening on port 8080")
-	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from foo-api, %s! You were doing %s %s\n", r.RemoteAddr, r.Method, r.RequestURI)
-	}))
+	err := srv.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }

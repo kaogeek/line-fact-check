497e3e14f307d7f797903631ba1bf6f7fa0cf4cd
Errortime <larb26656@gmail.com>
2025-06-19 09:01:56 +0700

    feat: init project

---
+++ backoffice-webapp/src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="p-4 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div>
+          <div className="text-2xl font-medium text-black ">
+            Tailwind & Shadcn
+          </div>
+          <p className="text-gray-500">Demonstration Component</p>
+        </div>
+        <Button variant="default">Click Me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;

69763288319802234dee46e90914f13a0e5ae3a1
Errortime <larb26656@gmail.com>
2025-06-19 19:33:03 +0700

    chore: reformat code all files

--- backoffice-webapp/src/App.tsx
+++ backoffice-webapp/src/App.tsx
@@ -1,14 +1,12 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   return (
     <>
       <div className="p-4 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
         <div>
-          <div className="text-2xl font-medium text-black ">
-            Tailwind & Shadcn
-          </div>
+          <div className="text-2xl font-medium text-black ">Tailwind & Shadcn</div>
           <p className="text-gray-500">Demonstration Component</p>
         </div>
         <Button variant="default">Click Me</Button>

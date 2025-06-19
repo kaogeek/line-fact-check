709f2c836d24d25e337536eba49bad73e5716391
Luckytime1996 <38021202+larb26656@users.noreply.github.com>
2025-06-19 20:37:14 +0700

    feat: init backoffice web app
    * feat: init project
    
    * chore: reformat code all files
    
    * ci: init lefthook for use as git hook
    
    * chore: update lefthook configuration for linting
    
    * fix: correct root path in lefthook configuration
    
    * chore: test git hook
    
    * feat: init route system

---
+++ backoffice-webapp/src/main.tsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import RootLayout from './layouts/RootLayout.tsx';
+import DashboardPage from './pages/dashboard/DashboardPage.tsx';
+import HomePage from './pages/home/HomePage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/dashboard',
+        element: <DashboardPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);

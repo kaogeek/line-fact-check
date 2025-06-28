08c3ab8b7979d297cd7254d5bb7f2b21526ffd61
Prem Phansuriyanon <artdrawin@gmail.com>
2025-06-29 01:01:04 +0700

    fix: lint gci, and try using factcheck struct in api

--- factcheck/cmd/api/main.go
+++ factcheck/cmd/api/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-
 	"github.com/kaogeek/line-fact-check/pillars"
+
+	"github.com/kaogeek/line-fact-check/factcheck"
 )
 
 func main() {
@@ -26,6 +28,34 @@ func main() {
 	r.Handle("/", pillars.HandlerEcho(name))
 	r.Handle("/health", pillars.HandlerOk(name))
 
+	r.Get("/topic", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		j, err := json.Marshal(map[string]any{
+			"topic": factcheck.Topic{
+				ID:        "some-topic-id",
+				Name:      "TOPIC-FOOBAR",
+				Status:    factcheck.StatusTopicResolved,
+				Result:    "this is a fake nees",
+				CreatedAt: time.Now(),
+				UpdatedAt: nil,
+			},
+			"client": map[string]any{
+				"uri":        r.RequestURI,
+				"url":        r.URL.String(),
+				"user-agent": r.UserAgent(),
+				"host":       r.Host,
+			},
+		})
+		if err != nil {
+			contentTypeText(w.Header())
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		contentTypeJSON(w.Header())
+		w.WriteHeader(200)
+		w.Write(j)
+	}))
+
 	srv := http.Server{
 		Addr:         addr,
 		Handler:      r,
@@ -39,4 +69,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func contentTypeJSON(h http.Header) {
+	h.Add("Content-Type", "application/json; charset=utf-8")
+}
+
+func contentTypeText(h http.Header) {
+	h.Add("Content-Type", "text/plain")
 }

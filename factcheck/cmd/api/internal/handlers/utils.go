e2b780ee3055501c8a54d54f87a43ca19199e98c
Prem Phansuriyanon <artdrawin@gmail.com>
2025-07-06 00:47:11 +0700

    generic handlers

--- factcheck/cmd/api/internal/handlers/utils.go
+++ factcheck/cmd/api/internal/handlers/utils.go
@@ -1,11 +1,61 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
+
+func create[T any](
+	w http.ResponseWriter,
+	r *http.Request,
+	repo interface {
+		Create(context.Context, T) (T, error)
+	},
+) {
+	data, err := decode[T](r)
+	if err != nil {
+		errBadRequest(w, err.Error())
+		return
+	}
+	created, err := repo.Create(r.Context(), data)
+	if err != nil {
+		errInternalError(w, err.Error())
+		return
+	}
+	replyJson(w, created, http.StatusCreated)
+}
+
+func list[T any](
+	w http.ResponseWriter,
+	r *http.Request,
+	repo interface {
+		List(context.Context) ([]T, error)
+	},
+) {
+	l, err := repo.List(r.Context())
+	if err != nil {
+		errInternalError(w, err.Error())
+		return
+	}
+	replyJson(w, l, http.StatusOK)
+}
+
+func decode[T any](r *http.Request) (T, error) {
+	var zero T
+	b, err := body(r)
+	if err != nil {
+		return zero, err
+	}
+	var t T
+	err = json.Unmarshal(b, &t)
+	if err != nil {
+		return zero, err
+	}
+	return t, nil
+}
 
 func body(r *http.Request) ([]byte, error) {
 	var b []byte
@@ -16,9 +66,17 @@ func body(r *http.Request) ([]byte, error) {
 	return b, nil
 }
 
-// replyJson marshals data into JSON string before writing response.
+// replyJson calls replyJsonError, and on non-nil error, writes 500 response
+func replyJson(w http.ResponseWriter, data any, status int) {
+	err := replyJsonError(w, data, status)
+	if err != nil {
+		errInternalError(w, err.Error())
+	}
+}
+
+// replyJsonError marshals data into JSON string before writing response.
 // If marshaling failed, the response is left untouched and the error is returned.
-func replyJson(w http.ResponseWriter, data any, status int) error {
+func replyJsonError(w http.ResponseWriter, data any, status int) error {
 	j, err := json.Marshal(data)
 	if err != nil {
 		return err

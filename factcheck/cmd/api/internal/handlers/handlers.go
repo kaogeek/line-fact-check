db8d0759215c56292005116dd720d2383ea93a26
Prem Phansuriyanon <artdrawin@gmail.com>
2025-07-06 00:47:11 +0700

    generic handlers

--- factcheck/cmd/api/internal/handlers/handlers.go
+++ factcheck/cmd/api/internal/handlers/handlers.go
@@ -1,11 +1,8 @@
 package handlers
 
 import (
-	"encoding/json"
-	"log/slog"
 	"net/http"
 
-	"github.com/kaogeek/line-fact-check/factcheck"
 	"github.com/kaogeek/line-fact-check/factcheck/internal/repo"
 )
 
@@ -14,39 +11,9 @@ type handler struct {
 }
 
 func (h *handler) CreateTopic(w http.ResponseWriter, r *http.Request) {
-	b, err := body(r)
-	if err != nil {
-		errBadRequest(w, err.Error())
-		return
-	}
-	var topic factcheck.Topic
-	err = json.Unmarshal(b, &topic)
-	if err != nil {
-		errBadRequest(w, err.Error())
-		return
-	}
-	created, err := h.topics.Create(r.Context(), topic)
-	if err != nil {
-		errInternalError(w, err.Error())
-		return
-	}
-	err = replyJson(w, created, http.StatusCreated)
-	if err != nil {
-		errInternalError(w, err.Error())
-		return
-	}
+	create(w, r, h.topics)
 }
 
 func (h *handler) ListTopics(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	list, err := h.topics.List(ctx)
-	if err != nil {
-		errInternalError(w, err.Error())
-		return
-	}
-	err = replyJson(w, list, http.StatusOK)
-	if err != nil {
-		errInternalError(w, err.Error())
-		return
-	}
+	list(w, r, h.topics)
 }

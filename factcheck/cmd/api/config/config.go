ca845ccab2f8bb522e81a01b94d840d6f6b19dd2
Prem Phansuriyanon <artdrawin@gmail.com>
2025-07-06 01:25:39 +0700

    config

--- factcheck/cmd/api/config/config.go
+++ factcheck/cmd/api/config/config.go
@@ -1,1 +1,26 @@
 package config
+
+type Postgres struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	DBName   string `mapstructure:"dbname"`
+}
+
+type Config struct {
+	Postgres Postgres `mapstructure:"postgres"`
+}
+
+func New() (*Config, error) {
+	// return hard-coded config for now
+	return &Config{
+		Postgres: Postgres{
+			Host:     "localhost",
+			Port:     5432,
+			User:     "postgres",
+			Password: "postgres",
+			DBName:   "factcheck",
+		},
+	}, nil
+}
--- factcheck/cmd/api/di/di.go
+++ factcheck/cmd/api/di/di.go
@@ -3,12 +3,14 @@ package di
 import (
 	"github.com/google/wire"
 
+	"github.com/kaogeek/line-fact-check/factcheck/cmd/api/config"
 	"github.com/kaogeek/line-fact-check/factcheck/cmd/api/internal/handlers"
 	"github.com/kaogeek/line-fact-check/factcheck/internal/repo"
 	"github.com/kaogeek/line-fact-check/factcheck/models/postgres"
 )
 
 var ProviderSetAPI = wire.NewSet(
+	config.New,
 	postgres.New,
 	repo.New,
 	handlers.New,

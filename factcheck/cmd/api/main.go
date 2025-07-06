3990b6e54801abef5435e3e18a3e1cf82d9cc19b
Prem Phansuriyanon <artdrawin@gmail.com>
2025-07-07 01:44:21 +0700

    messages handler

--- factcheck/cmd/api/main.go
+++ factcheck/cmd/api/main.go
@@ -27,22 +27,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer func() {
+		slog.Info("server cleaning up")
+		cleanup()
+		slog.Info("server cleanup completed, exiting...")
+	}()
 
-	topics := chi.NewMux()
+	topics, messages := chi.NewMux(), chi.NewMux()
 	topics.Get("/", container.Handler.ListTopics)
 	topics.Get("/{id}", container.Handler.GetTopicByID)
 	topics.Post("/", container.Handler.CreateTopic)
 	topics.Delete("/{id}", container.Handler.DeleteTopicByID)
+	messages.Get("/{id}", container.Handler.ListMessagesByTopicID)
+	messages.Post("/", container.Handler.CreateMessage)
+	messages.Delete("/", container.Handler.DeleteMessageByID)
 
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-
 	r.Handle("/", pillars.HandlerEcho(name))
 	r.Handle("/health", pillars.HandlerOk(name))
 	r.Mount("/topics", topics)
+	r.Mount("/messages", messages)
 
 	srv := http.Server{
 		Addr:         addr,
@@ -52,7 +60,7 @@ func main() {
 	}
 
 	go func() {
-		slog.Info("starting server", "addr", addr)
+		slog.Info("server starting", "addr", addr)
 		err := srv.ListenAndServe()
 		if err != nil && !errors.Is(err, http.ErrServerClosed) {
 			slog.Error("server error", "error", err)
@@ -63,15 +71,13 @@ func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	slog.Info("shutting down server...")
+	slog.Info("server shutting down...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	timeout := time.Second * 30
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
-		slog.Error("server forced to shutdown", "error", err)
+		slog.Error("server forced to shutdown", "timeout", timeout.String(), "error", err)
 	}
-
-	cleanup()
-	slog.Info("server exited")
 }

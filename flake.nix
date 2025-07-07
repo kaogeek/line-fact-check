c1fc9eb4f5ae970091267c783e36a3ce926a1eef
Prem Phansuriyanon <artdrawin@gmail.com>
2025-07-08 03:25:47 +0700

    fix: cmd not found

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@ rec {
         docker-postgres-integration-test = pkgs.dockerTools.buildImage {
           name = "postgres-integration-test";
           tag = version;
-          copyToRoot = [ pkgs.bash pkgs.coreutils pkgs.postgresql_16 pkgs.shadow ];
+          copyToRoot = [ pkgs.bash pkgs.coreutils pkgs.postgresql_16 pkgs.su ];
           config = {
             Env = [
               "POSTGRES_USER=postgres"

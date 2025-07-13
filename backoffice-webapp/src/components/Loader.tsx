0d2e38b1513584903e72ca27e02ed68a4dd98634
Luckytime1996 <larb26656@gmail.com>
2025-07-14 01:11:28 +0700

    fix: loader is missing

--- backoffice-webapp/src/components/Loader.tsx
+++ backoffice-webapp/src/components/Loader.tsx
@@ -1,14 +1,9 @@
 import { PulseLoader } from 'react-spinners';
-import { useLoader } from '../hooks/useLoader';
 
 interface LoaderProps {
   className?: string;
 }
 
 export default function Loader({ className }: LoaderProps) {
-  const { isLoading } = useLoader();
-
-  if (!isLoading) return null;
-
   return <PulseLoader className={className} color="var(--color-primary)" />;
 }

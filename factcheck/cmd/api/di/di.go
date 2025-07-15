96c7d719bfe22be174f85466858d9e3aedff3bcd
Prem Phansuriyanon <artdrawin@gmail.com>
2025-07-16 00:34:08 +0700

    prevent race cond in tests with -p=1, and use tee in gh actions

--- factcheck/cmd/api/di/di.go
+++ factcheck/cmd/api/di/di.go
@@ -2,9 +2,6 @@
 package di
 
 import (
-	"context"
-	"log/slog"
-
 	"github.com/kaogeek/line-fact-check/factcheck/cmd/api/config"
 	"github.com/kaogeek/line-fact-check/factcheck/cmd/api/internal/handler"
 	"github.com/kaogeek/line-fact-check/factcheck/cmd/api/internal/server"
@@ -20,8 +17,6 @@ type Container struct {
 	Handler         handler.Handler
 	Server          server.Server
 }
-
-type ContainerTest Container
 
 func New(
 	conf config.Config,
@@ -39,49 +34,4 @@ func New(
 		Handler:         handler,
 		Server:          server,
 	}
-}
-
-func NewTest(
-	conf config.Config,
-	conn postgres.DBTX,
-	querier postgres.Querier,
-	repo repo.Repository,
-	handler handler.Handler,
-	server server.Server,
-) (
-	ContainerTest,
-	func(),
-) {
-	clearData(conn, "init")
-	cleanup := func() {
-		clearData(conn, "teardown")
-	}
-	return ContainerTest(New(
-		conf,
-		conn,
-		querier,
-		repo,
-		handler,
-		server,
-	)), cleanup
-}
-
-func clearData(conn postgres.DBTX, stage string) {
-	slog.Warn("Clearing all data from database", "stage", stage)
-
-	ctx := context.Background()
-	_, err := conn.Exec(ctx, "DELETE FROM topics")
-	if err != nil {
-		panic(err)
-	}
-	_, err = conn.Exec(ctx, "DELETE FROM messages")
-	if err != nil {
-		panic(err)
-	}
-	_, err = conn.Exec(ctx, "DELETE FROM user_messages")
-	if err != nil {
-		panic(err)
-	}
-
-	slog.Warn("Cleared all data from database", "stage", stage)
 }

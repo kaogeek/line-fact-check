18f08c280dda3fabd0f31f96a2dc183da03d8e31
Prem Phansuriyanon <artdrawin@gmail.com>
2025-07-24 03:31:54 +0700

    add healthcheck

---
+++ factcheck/cmd/healthcheck/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	addr := os.Getenv("FACTCHECKAPI_LISTEN_ADDRESS")
+	timeoutEnv := os.Getenv("FACTCHECKAPI_TIMEOUTMS_WRITE")
+	timeoutMs, err := strconv.Atoi(timeoutEnv)
+	if err != nil {
+		timeoutMs = 2000
+	}
+	c := http.Client{
+		Timeout: time.Millisecond * time.Duration(timeoutMs),
+	}
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost%s/health", addr), nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		panic(resp.StatusCode)
+	}
+}

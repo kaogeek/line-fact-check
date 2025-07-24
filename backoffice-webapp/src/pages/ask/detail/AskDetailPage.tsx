f7a969e170d2c2a627a331ce7cd1f3654bd9fe9d
Luckytime1996 <larb26656@gmail.com>
2025-07-25 00:01:11 +0700

    fix: build error on backoffice and add build in precommit hook for make sure commit can build

--- backoffice-webapp/src/pages/ask/detail/AskDetailPage.tsx
+++ backoffice-webapp/src/pages/ask/detail/AskDetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { ChatMessage } from '../type';
 import { TYH4 } from '@/components/Typography';
 import ChatScreen from '@/components/chat/ChatScreen';

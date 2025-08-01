e7169b7068b2dfcbcbb6340d49cb0cdfc7cabaee
Prem Phansuriyanon <artdrawin@gmail.com>
2025-08-02 01:06:52 +0700

    fix: lint

--- factcheck/internal/core/submit.go
+++ factcheck/internal/core/submit.go
@@ -28,7 +28,7 @@ func (s ServiceFactcheck) Submit(
 		return factcheck.MessageV2{}, factcheck.MessageGroup{}, nil, errors.New("empty message text submitted")
 	}
 
-	slog.Info("got submission", "text", text, "topic_id", topicID)
+	slog.InfoContext(ctx, "got submission", "text", text, "topic_id", topicID)
 	meta := factcheck.Metadata[factcheck.UserInfo]{
 		Type: factcheck.TypeMetadataUserInfo,
 		Data: user,
@@ -69,12 +69,12 @@ func (s ServiceFactcheck) Submit(
 	group, err := s.repo.MessageGroups.GetBySHA1(ctx, textSHA1, withTx)
 	if err != nil {
 		if !repo.IsNotFound(err) {
-			return factcheck.MessageV2{}, factcheck.MessageGroup{}, nil, fmt.Errorf("error finding group based on sha1 hash '%s': %s", textSHA1, err)
+			return factcheck.MessageV2{}, factcheck.MessageGroup{}, nil, fmt.Errorf("error finding group based on sha1 hash '%s': %w", textSHA1, err)
 		}
 
 		// If not found, we'll create a new group for it.
 		// But the group will not have topicID - to be assigned topic by admin
-		slog.Info("pre-creating new group", "sha1", textSHA1)
+		slog.InfoContext(ctx, "pre-creating new group", "sha1", textSHA1)
 		group = factcheck.MessageGroup{
 			ID:        utils.NewID().String(),
 			Status:    factcheck.StatusMGroupPending,
@@ -89,7 +89,7 @@ func (s ServiceFactcheck) Submit(
 		)
 		group, err = s.repo.MessageGroups.Create(ctx, group, withTx)
 		if err != nil {
-			slog.Error("error pre-creating group",
+			slog.ErrorContext(ctx, "error pre-creating group",
 				"gid", group.ID,
 				"sha1", textSHA1,
 				"err", err,

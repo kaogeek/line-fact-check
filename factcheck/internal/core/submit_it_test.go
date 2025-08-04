c36db14ad4b795e8754ef3ce2700f96dca59c63b
Prem Phansuriyanon <artdrawin@gmail.com>
2025-08-04 12:03:02 +0700

    update test

--- factcheck/internal/core/submit_it_test.go
+++ factcheck/internal/core/submit_it_test.go
@@ -22,18 +22,24 @@ func TestSubmit(t *testing.T) {
 		repo := container.Repository
 
 		msgText := "text-it-test"
+		msgText2 := "text-it-test-2"
 		user := factcheck.UserInfo{
 			UserType: factcheck.TypeUserMessageLINEChat,
 			UserID:   "it-test",
 		}
 
-		msg, group, topic, err := service.Submit(ctx, user, msgText, "")
+		msg1, group, topic, err := service.Submit(ctx, user, msgText, "")
 		if err != nil {
 			t.Fatal(err)
+		}
+		if msg1.GroupID != group.ID {
+			t.Fatalf("unexpected message group ID '%s', expecting '%s'", msg1.GroupID, group.ID)
 		}
 		if topic != nil {
 			t.Fatal("unexpected topic", topic)
 		}
+
+		// Save group 1 values
 		groupID := group.ID
 		groupSHA1 := group.TextSHA1
 
@@ -47,11 +53,29 @@ func TestSubmit(t *testing.T) {
 		if topic != nil {
 			t.Fatal("unexpected topic", topic)
 		}
+		if msg2.GroupID != group.ID {
+			t.Fatalf("unexpected message group ID '%s', expecting '%s'", msg1.GroupID, groupID)
+		}
 		if group.ID != groupID {
 			t.Fatal("unexpected group ID", groupID, group.ID)
 		}
 		if group.TextSHA1 != groupSHA1 {
 			t.Fatal("unexpected group SHA1", groupSHA1, group.TextSHA1)
+		}
+
+		// Different text, different hash, different group
+		msg3, group, topic, err := service.Submit(ctx, user, msgText2, "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if topic != nil {
+			t.Fatal("unexpected topic", topic)
+		}
+		if msg3.GroupID != group.ID {
+			t.Fatalf("unexpected message group ID '%s', expecting '%s'", msg1.GroupID, groupID)
+		}
+		if group.ID == groupID {
+			t.Fatal("expected group IDs to be different", groupID)
 		}
 
 		msgs, err := repo.MessagesV2.ListByGroup(ctx, groupID)
@@ -61,7 +85,7 @@ func TestSubmit(t *testing.T) {
 		if len(msgs) != 2 {
 			t.Fatal("unexpected length", len(msgs))
 		}
-		if msgs[0].ID != msg.ID {
+		if msgs[0].ID != msg1.ID {
 			t.Fatal("unexpected msgs[0].ID")
 		}
 		if msgs[1].ID != msg2.ID {
